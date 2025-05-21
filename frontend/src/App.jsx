058f132936901a72beec699f435535990fcd89b6
Yashavanth G Gowda <yashavanthgs2005@gmail.com>
2025-05-21 14:09:44 +0530

    Initaial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// rafce
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+
+  return (
+    <div>
+      <Navbar/>
+    </div>
+    
+  )
+}
+
+export default App
